efee6cd3e88d17f7121f141164c7f3ee301d70fa
minhluu2608 <minhluu2608@gmail.com>
2022-08-20 17:59:03 +0700

    add payment_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:MobileApp_LVTN/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:MobileApp_LVTN/screens/home_screen.dart';
 import 'package:MobileApp_LVTN/screens/login_screen.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async{
   await Hive.initFlutter();
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey =
+    "pk_test_51LYlhhBpklmPdZX2H96DE6Eb5LiY6579BgmBXclgTycNcB62eeUT1qZ1qvDjjKAWLSTDRWXXkjDTxRIrMfUVbK9B00tX6O8uwC";
+  await Stripe.instance.applySettings();
   runApp(MyApp());
 }
 
