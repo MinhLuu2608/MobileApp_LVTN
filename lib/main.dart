5e198fc397a8a38d7a75d3376bf47ba4cfdbce92
minhluu2608 <minhluu2608@gmail.com>
2022-08-20 17:41:51 +0700

    Chinh sua vai thu

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:MobileApp_LVTN/screens/home_screen.dart';
 import 'package:MobileApp_LVTN/screens/login_screen.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async{
   await Hive.initFlutter();
